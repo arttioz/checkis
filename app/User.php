0fbcb4d73d66f23a7004d5ba4fbe795142a08a98
Benz <wasinjansamut@gmail.com>
2020-05-07 14:07:49 +0700

    For test
    ทดสอบไง

--- app/User.php
+++ app/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    /** Test Wasin */
+
     /**
      * The attributes that should be hidden for arrays.
      *
