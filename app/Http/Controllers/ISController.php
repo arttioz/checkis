77f8c6c5be4882e83f93c2571bf63125229394d7
Arttioz <arttioz@gmail.com>
2020-05-13 20:58:53 +0700

    Check solider and polish name for job 02

--- app/Http/Controllers/ISController.php
+++ app/Http/Controllers/ISController.php
@@ -23,50 +23,106 @@ class ISController extends Controller
     }
 
     public static  function runCheckData($month ,$year, $hospCode = ""){
-    set_time_limit(1000);
+        set_time_limit(1000);
 
-        $isData = ISOnline::whereMonth("lastupdate",$month)->whereYear("lastupdate",$year)->get();
+
+        $preNames = [];
+        $isData = ISOnline::whereMonth("lastupdate",$month)->whereYear("lastupdate",$year)->limit(10000)->get();
         foreach ($isData as $row){
             self::checkISData($row);
+
+            $key_name = $row->prename . "_". $row->sex;
+
+            if( array_key_exists($key_name,$preNames) ){
+                $preNames[$key_name] ++;
+            }else{
+                $preNames[$key_name] = 1;
+            }
+        }
+
+        foreach ($preNames as $key => $count) {
+            echo  $key. " : " . $count . "<br>";
         }
     }
+
 
     public static  function checkISData(ISOnline $row){
 
-       if (self::checkFrontNameWithSex($row)){
-           echo $row->id . " ". $row->prename. " ". $row->sex. " : error  checkFrontNameWithSex <br>";
-       }
-        if (self::checkDriverAge($row)){
-            echo $row->id . " : error  checkDriverAge <br>";
+
+        // ตรวจสอบ คำนำหน้าไม่ตรงกับเพศ
+        if (self::checkFrontNameWithSex($row)){
+            echo $row->id . " ". $row->prename. " ". $row->sex. " : error  checkFrontNameWithSex <br>";
+        }
+
+        //  ตรวจสอบ คำหน้าหน้า อาชีพเป็น หทาย ตำรวจ แต่ อาชีพไม่ใช้หทาย ตำรวจ
+        if (self::checkFrontNameWithJob($row)){
+            echo $row->id . " ". $row->prename. " ". $row->sex. " : error คำหน้าหน้า อาชีพเป็น หทาย ตำรวจ แต่ อาชีพไม่ใช้หทาย ตำรวจ <br>";
         }
 
     }
 
+
+    // ตรวจสอบ คำนำหน้าไม่ตรงกับเพศ
     public static function checkFrontNameWithSex(ISOnline $row){
 
-        $maleFrontName = ['นาย','ด.ช.' , 'Mr.' , 'พระ'];
+        $maleFrontName = ['นาย','ด.ช.' , 'Mr' , 'พระ'];
         $male_sex = 1;
 
-        $femaleFrontName = ['นาง','น.ส.','ด.ญ.' ,'นส.' , 'ดญ.' ,'พันตรีหญิง'];
+        $femaleFrontName = [ 'Ms','Mrs','นาง','น.ส.','ด.ญ.' ,'นส.' , 'ดญ.' ,'หญิง', "แม่"];
         $female_sex = 2;
 
         $prename = $row->prename;
         $sex = $row->sex;
 
-        if($sex == $female_sex){
-            if (!in_array($prename,$femaleFrontName)){
+        if(self::isContainArray($prename,$femaleFrontName)){
+            if ($sex != $female_sex){
+                //  คำหน้าหน้าหญิง แต่ เพศไม่หญิง
                 return true;
             }else{
                 return false;
             }
 
-        }else{
-            if (!in_array($prename,$maleFrontName)){
+        }else if (self::isContainArray($prename,$maleFrontName))
+        {
+            if ($sex != $male_sex){
+                //  คำนำหน้าชาย แต่ เพศไม่ชาย
                 return true;
             }else{
                 return false;
             }
         }
+
+        return false;
+    }
+
+    public static function isContainArray($check, $arrayWord){
+
+        foreach ($arrayWord as $word){
+            if (strpos(strtolower($word) , strtolower($check) ) !== false) {
+               return true;
+            }
+        }
+        return false;
+    }
+
+    // ตรวจสอบ คำหน้าหน้า อาชีพเป็น หทาย ตำรวจ แต่ อาชีพไม่ใช้หทาย ตำรวจ
+    public static function checkFrontNameWithJob(ISOnline $row){
+
+        $polish_soilder_prename = ['พล','ส.อ.' , 'ร.ท.' , 'เรือ' , 'ตำรวจ', "สิบ","ร้อย" , "พัน"];
+        $occuCheck = 2;
+
+        $prename = $row->prename;
+        $OCCU = (int) $row->OCCU;
+
+        foreach ($polish_soilder_prename as $checkPrename){
+            if (strpos($checkPrename, $prename) !== false) {
+                if($OCCU != $occuCheck){
+                    // คำหน้าหน้า อาชีพเป็น หทาย แต่ อาชีพไม่ใช้ทหาร
+                    return true;
+                }
+            }
+        }
+        return false;
 
     }
 
